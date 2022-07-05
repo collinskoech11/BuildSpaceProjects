49bcd59934e62130a9ff3d0b1a4ad56b00656ece
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-07-05 15:35:10 +0300

    update contract.sol

--- WavePortal/my-wave-portal/contracts/WavePortal.sol
+++ WavePortal/my-wave-portal/contracts/WavePortal.sol
@@ -4,12 +4,33 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;// state variable stored permanently on the blockchain
-    constructor() {
-        console.log("Yo yo, Im a contract and IM smart");
+    event NewWave(address indexed from, uint256 timestamp, string message);//create an emit to store transaction logs
+    struct Wave {//hashmap lookalike stores key value pairs
+        address waver;//address of theuser who waved
+        string message;// message taht the user sent 
+        uint256 timestamp;// timestamp when the user waved
     }
-    function wave() public {
-      totalWaves += 1;
+    Wave[] waves;// declare variables waves that lets me store an array of structs
+    constructor () payable {
+        console.log("We have been constructed!");
+    }
+    function wave(string memory _message) public {//passed in the message that the user created 
+        totalWaves += 1;
         console.log("%s has waved!", msg.sender);// Know who exactly called the function
+        waves.push(Wave(msg.sender, _message, block.timestamp));//store wave data to the aray waves 
+        emit NewWave(msg.sender, block.timestamp, _message);// store the actual values of the transaction logs
+
+        uint prizeAmount = 0.0001 ether;
+        require(
+            prizeAmount <= address(this).balance, //check that prize amount is less than the balance of the contract
+            "Tying to wothdraw more than the contract has"
+        );
+        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+        require(success, "Failed to withdraw money from contract");
+
+    }
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
     function getTotalWaves() public view returns (uint256) {
         console.log("We have %d total waves!", totalWaves);
--- WavePortal/my-wave-portal/scripts/run.js
+++ WavePortal/my-wave-portal/scripts/run.js
@@ -1,25 +1,47 @@
 const main = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners()
+    // const [owner, randomPerson] = await hre.ethers.getSigners()
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");//compiem our contract store files under artifacts
-    const waveContract = await waveContractFactory.deploy();//hardhat creates  local ethereum network
+    const waveContract = await waveContractFactory.deploy({
+        value: hre.ethers.utils.parseEther('0.1'),
+    });//hardhat creates  local ethereum network
     await waveContract.deployed();//wait till the contract is deployed 
+    console.log("Contract address : ", waveContract.address);
+    // console.log("Contract deployed by", owner.address);//address of the person deploying the cpontract
+
+    // let waveCount;
+    // waveCount = await waveContract.getTotalWaves();//check wave count total
+    // console.log(waveCount.toNumber());
+    let contractBalance= await hre.ethers.provider.getBalance(
+        waveContract.address
+    );
+
+    console.log(
+        "Contract Balance :",
+        hre.ethers.utils.formatEther(contractBalance)
+    );
+
+    //send a few waves
+
+    let waveTxn = await waveContract.wave("Tingiza Mti");//make a new wave
+    await waveTxn.wait();//wait for the transaction to be mined 
 
 
-    console.log("Contract deployed to : ", waveContract.address);
-    console.log("Contract deployed by", owner.address);//address of the person deploying the cpontract
 
-    let waveCount = 0;
-    waveCount = await waveContract.getTotalWaves();//check wave count total
+    contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+    console.log("Contract Balance:",
+        hre.ethers.utils.formatEther(contractBalance)
+    )
 
-    let waveTxn = await waveContract.wave();//make a new wave
-    await waveTxn.wait();
+    // waveCount = await waveContract.getTotalWaves();//recheck the number of waves
 
-    waveCount = await waveContract.getTotalWaves();//recheck the number of waves
+    // const [_, randomPerson] = await hre.ethers.getSigners();
+    // // waveTxn = await waveContract.connect(randomPerson).wave();//random person makes a new wave 
+    // waveTxn = await waveContract.connect(randomPerson).wave("Another banga");
+    // await waveTxn.wait();
 
-    waveTxn = await waveContract.connect(randomPerson).wave();//random person makes a new wave 
-    await waveTxn.wait();
-
-    waveCount = await waveContract.getTotalWaves();
+    let allWaves = await waveContract.getAllWaves();
+    // waveCount = await waveContract.getTotalWaves();
+    console.log(allWaves);
 };
 
 const runMain = async () => {
