29745666a125fccf65c09d2c804f6a2b0b25fb67
collinskoech11 <collinskoechck34@gmail.com>
2022-07-16 17:57:32 +0300

    inittialize nft deploy.js

--- epic-nfts/contracts/MyEpicNFT.sol
+++ epic-nfts/contracts/MyEpicNFT.sol
@@ -1,10 +1,31 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.1;
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-contract MyEpicNFT {
-    constructor() {
-        console.log("NFT Contract");
+//inherit the contract we just imported. This means we'll have access to the inherited contract methods
+// Openzeplin implements the NFT standard for us and lets us write logic on top of it to customize it 
+contract MyEpicNFT  is ERC721URIStorage {
+    // inherit from openzeplin contract => importing functions for us to use
+    // Open zeplin helps us keep track of the tokenIDs
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    //pass name of our NFT tokens and its symbol.
+    constructor() ERC721 ("SquareNFT", "SQUARE") {
+        console.log("This is the nft contract");
+    }
+    //A function out user will hit to get their NFT
+    function makeAnEpicNft() public {
+        // Get the current token ID's, this starts with 0
+        uint256 newItemId = _tokenIds.current();//_nftTokenId is a state variable which means if altered the value is stored on the contract directly
+        //Actually mint the NFT to the sender using msg.sender
+        _safeMint(msg.sender, newItemId);// mint the ntf with the id of newItemId
+        //set the NFT data to the
+        _setTokenURI(newItemId, "../Data/data.json");
+        console.log("An NFT with ID %s has been minted to %s", newItemId, msg.sender);
+        //increment the counter for when the next NFT is minted.
+        _tokenIds.increment();//move to the next NFT
     }
 }
--- epic-nfts/scripts/run.js
+++ epic-nfts/scripts/run.js
@@ -4,6 +4,15 @@ const main = async () => {
     const nftContract = await nftContractFactory.deploy();//create local network for deploying destroy when done 
     await nftContract.deployed();//wait until the contract is mined and deployed to the local blockchain
     console.log("Contract deployed to : ", nftContract.address)
+    // Call the function
+    let txn  = await nftContract.makeAnEpicNft();
+    // wait fir it to be Mined
+    await txn.wait();
+
+    // Mint another NFT transaction
+    txn = await  nftContract.makeAnEpicNft();
+    // wait for it to be Mined
+    await txn.wait();
    };
 
 const runMain = async () => {
