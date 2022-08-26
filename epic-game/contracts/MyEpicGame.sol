e3d618895d51bbce41e619e0c4d86ebe28aec9ec
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-08-26 15:51:49 +0300

    added sample characters via run.js

--- epic-game/contracts/MyEpicGame.sol
+++ epic-game/contracts/MyEpicGame.sol
@@ -1,10 +1,76 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+// Helper functions OpenZeppelin provides.
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
 import "hardhat/console.sol";
 
+
 contract MyEpicGame {
-    constructor() {
-        console.log("THIS is my game contract");
+
+    struct CharacterAttributes {
+        uint characterIndex;
+        string name;
+        string ImageURI;
+        uint hp;
+        uint maxHp;
+        uint attackDamage;
+    }
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+
+    CharacterAttributes[] defaultCharacters;
+
+    mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
+
+    mapping(address => uint256) public nftHolders;
+
+    constructor(
+        string[] memory characterNames,
+        string[] memory characterImageURIs,
+        uint[] memory characterHp,
+        uint[] memory characterAttackDmg
+    )
+    ERC721("Heroes", "HERO")
+    {
+        for (uint i=0; i<characterNames.length; i+=1){
+            defaultCharacters.push(CharacterAttributes({
+                characterIndex: i,
+                name: characterNames[i],
+                ImageURI: characterImageURIs[i],
+                hp: characterHp[i],
+                maxHp: characterHp[i],
+                attackDamage: characterAttackDmg[i]
+            }));
+
+            CharacterAttributes memory c = defaultCharacters[i];
+            console.log("Done Initializing %s w/ HP %s", c.name, c.hp, c.ImageURI);
+        }
+        _tokenIds.increment();
+    }
+
+    function mintCharacterNFT(uint _characterIndex) external {
+        uint256 newItemId = _tokenIds.curent();
+        _safeMint(msg.sender, newItemId);
+
+        nftHolderAttributes[newItemId] =  CharacterAttributes({
+            characterIndex: _characterIndex,
+            name: defaultCharacters[_characterIndex].name,
+            imageURI: defaultCharacters[_characterIndex].imageURI,
+            hp: defaultCharacters[_characterIndex].hp,
+            maxHp: defaultCharacters[_characterIndex].maxHp,
+            attackDamage: defaultCharacters[_characterIndex].attackDamage
+        });
+        console.log("Minted NFT w/ tokenId %s and characterIndex %s", newItemId, _characterIndex);
+        nftHolders[msg.sender] = newItemId;
+
+        _tokenIds.increment();
     }
 }
--- epic-game/scripts/run.js
+++ epic-game/scripts/run.js
@@ -1,7 +1,12 @@
 
 const main = async () => {
     const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');//compile MyEpicGame.sol contract
-    const gameContract = await gameContractFactory.deploy()// create local ethereum network
+    const gameContract = await gameContractFactory.deploy(
+        ["Leo", "Aang", "Pikachu"],
+        ["https://i.imgur.com/pKd5Sdk.png","https://i.imgur.com/xVu4vFL.png","https://i.imgur.com/WMB6g9u.png"],
+        [100, 200, 300],
+        [100, 50, 25],
+    );// create local ethereum network
     await  gameContract.deployed();//wait for tx to be mined
     console.log("Contract deployed to:", gameContract.address);//log the address of the deployed smart contract
 };
