d9f8dd67d51d27e70c0133a483a00189739c6ca0
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-08-29 13:52:12 +0300

    set app initial state

--- epic-game/frontend/src/App.js
+++ epic-game/frontend/src/App.js
@@ -1,14 +1,15 @@
 import React, {useEffect, useState} from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
-
+import SelectCharacter from './Components/SelectCharacter';
 // Constants
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-  const [currentAccount, setCurrentAccount] = useState("")
-  const checkIfWalletIsConnected = () => {
+  const [currentAccount, setCurrentAccount] = useState(null);
+  const [characterNFT, setCharacterNFT] = useState(null);
+  const checkIfWalletIsConnected = async() => {
     try{
     const {ethereum } = window;
     if(!ethereum) {
@@ -31,7 +32,48 @@ const App = () => {
       console.log("err",error)
     }
   };
+  const renderContent = () => {
+    //scenio one
+    if(!currentAccount){
+      return (
+        <div className="connect-wallet-container">
+          <img
+            src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
+            alt="Monty Python Gif"
+          />
+          <button
+            className="cta-button connect-wallet-button"
+            onClick={connectWalletAction}
+          >
+            Connect Wallet To Get Started
+          </button>
+        </div>
+      )
+    } else if (currentAccount && !characterNFT) {
+      return <SelectCharacter setCharacterNFT={setCharacterNFT}/>;
+    }
+  };
 
+  const connectWalletAction = async () => {
+    try{
+      const {ethereum} = window;
+
+      if(!ethereum){
+        alert('Get Metamask');
+        return;
+      }
+      //fancy method to request access to account
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+      // Print the public address once we authorize metamask.
+      console.log('Connected', accounts[0]);
+      setCurrentAccount(accounts[0])
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  
   useEffect(() => {
     checkIfWalletIsConnected();
   }, []);
@@ -41,12 +83,7 @@ const App = () => {
         <div className="header-container">
           <p className="header gradient-text">⚔️ Metaverse Slayer ⚔️</p>
           <p className="sub-text">Team up to protect the Metaverse!</p>
-          <div className="connect-wallet-container">
-            <img
-              src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
-              alt="Monty Python Gif"
-            />
-          </div>
+          {renderContent()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
---
+++ epic-game/frontend/src/Components/SelectCharacter/index.js
@@ -0,0 +1,13 @@
+import Reat, {useEffect, useState} from 'react';
+import './SelectCharacter.css';
+
+
+const SelectCharacter = ({ setCharacterNFT }) => {
+    return (
+        <div className="select-character-container">
+            <h2>Mint Your Hero Choose Wisely.</h2>
+        </div>
+    )
+}
+
+export default SelectCharacter;
